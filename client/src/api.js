7df2329530fc26554657955e5a41d6a827510f5a
Sourav Karan <148673779+Sourav388@users.noreply.github.com>
2025-12-29 21:27:15 +0530

    Final fix for file extensions

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import API from "../api.js"
 
 // This checks if we are on the web or on our computer
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
